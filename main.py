5f2fcf2f3bef63a3d0e9d7e62adb21e2840767da
nathanieldorn <nathaniel.svc@pm.me>
2025-04-19 16:48:04 -0400

    Results report added

--- main.py
+++ main.py
@@ -1,11 +1,21 @@
-from stats import count_words
+from stats import count_words, sort_counts
 
 def get_book_text(book_path):
     with open(book_path) as f:
         book_contents = f.read()
     return book_contents
 
-def main ():
+def print_book ():
     print(get_book_text("books/frankenstein.txt"))
 
-count_words(get_book_text("books/frankenstein.txt"))
+def main():
+    path_of_book = "books/frankenstein.txt"
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {path_of_book}")
+    print("----------- Word Count ----------")
+    count_words(get_book_text(path_of_book))
+    print("--------- Character Count -------")
+    sort_counts(get_book_text(path_of_book))
+    print("============= END ===============")
+
+main()
--- stats.py
+++ stats.py
@@ -1,3 +1,25 @@
 def count_words(book_contents):
     num_words = len(book_contents.split())
-    print(f"{num_words} words found in the document")
+    print(f"Found {num_words} total words")
+
+def count_char(book_contents):
+    book_chars = {}
+    for char in book_contents.lower():
+        if char in book_chars:
+            book_chars[char] += 1
+        else:
+            book_chars[char] = 1
+    for char in book_chars:
+        print(f"'{char}': {book_chars[char]}")
+
+def sort_counts(book_contents):
+    book_chars = {}
+    for char in book_contents.lower():
+        if char in book_chars:
+            book_chars[char] += 1
+        else:
+            book_chars[char] = 1
+    sorted_book_chars = dict(sorted(book_chars.items(), key=lambda item: item[1], reverse=True))
+    for char in sorted_book_chars:
+        if char.isalpha():
+            print(f"{char}: {sorted_book_chars[char]}")
