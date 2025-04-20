da213343663c59a3b1a845dab98e693c8a7810c3
nathanieldorn <nathaniel.svc@pm.me>
2025-04-19 20:52:54 -0400

    Added additional books and filepath input

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
 from stats import count_words, sort_counts
+import sys
 
-def get_book_text(book_path):
-    with open(book_path) as f:
+def select_book():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    global book_path
+    book_path = sys.argv[1]
+
+def get_book_text(path):
+    with open(path) as f:
         book_contents = f.read()
     return book_contents
 
 def print_book ():
-    print(get_book_text("books/frankenstein.txt"))
+    print(get_book_text(book_path))
 
 def main():
-    path_of_book = "books/frankenstein.txt"
+    select_book()
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {path_of_book}")
+    print(f"Analyzing book found at {book_path}")
     print("----------- Word Count ----------")
-    count_words(get_book_text(path_of_book))
+    count_words(get_book_text(book_path))
     print("--------- Character Count -------")
-    sort_counts(get_book_text(path_of_book))
+    sort_counts(get_book_text(book_path))
     print("============= END ===============")
 
 main()
